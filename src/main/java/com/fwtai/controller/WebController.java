1711d286ba3b9a4b39bdf5aec0bd395a99a01b2e
gzstyp <444141300@qq.com>
2020-05-20 19:43:29 +0800

    构建websocket应用

---
+++ src/main/java/com/fwtai/controller/WebController.java
@@ -0,0 +1,20 @@
+package com.fwtai.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+@RestController
+public class WebController{
+
+    @GetMapping(value = {"index","/"})
+    public ResponseEntity<String> index(){
+        return ResponseEntity.ok("请求成功");
+    }
+
+    @GetMapping("page")
+    public ModelAndView page(){
+        return new ModelAndView("websocket");
+    }
+}
